48734652287a40f16b241baf3e481b107bbcfe8c
ken-mu <gas.010bl@gmail.com>
2018-10-03 21:22:57 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,6 @@
 #!/usr/bin
 
-expect -c "
 spawn sh /tmp/setup.sh
-expect \\\"Do you want to proceed with the installation? [default: yes] \\\"
-send \\\"yes\n\\\"
-"
+expect "Do you want to proceed with the installation? [default: yes] "
+send "yes\n"
+interact
