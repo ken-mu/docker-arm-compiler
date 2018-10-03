8f2a37515d08fc9a8f414c1b7c2df54343972edc
ken-mu <gas.010bl@gmail.com>
2018-10-03 23:15:18 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,7 +2,20 @@
 
 expect -c "
   spawn sh /tmp/setup.sh
-  expect \"default: yes] \"
+  set i 0
+  while {\$i <= 1000} {
+    send \"\n\"
+    incr i 1
+  }
+  expect {
+    timeout {exit 1}
+    \"Do you agree to the above terms and conditions? \"
+  }
   send \"yes\n\"
+  set i 0
+  while {\$i <= 2} {
+    send \"\n\"
+    incr i 1
+  }
   interact
 "
