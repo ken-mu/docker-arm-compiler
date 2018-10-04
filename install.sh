8e3e6eac49af8d706a84a6b794e2fa00e664b55e
ken-mu <gas.010bl@gmail.com>
2018-10-04 21:43:30 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,20 +2,20 @@
 
 expect -c "
   spawn sh /tmp/setup.sh
-  set i 0
-  while {\$i <= 1000} {
-    send \"\n\"
-    incr i 1
-  }
-  expect {
-    timeout {exit 1}
-    \"Do you agree to the above terms and conditions? \"
-  }
+  expect \"Do you want to proceed with the installation?\"
+  send \"\n\"
+
+  expect -regexp \"^.+More.+$\"
+  send \"q\"
+
+  expect \"Do you agree to the above terms and conditions\"
   send \"yes\n\"
-  set i 0
-  while {\$i <= 2} {
-    send \"\n\"
-    incr i 1
-  }
-  interact
+
+  expect \"Where would you like to install to\"
+  send \"\n\"
+
+  expect -regexp \"^.+does not exist, create.+$\"
+  send \"\n\"
+
+  expect eof
 "
